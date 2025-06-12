89c03d32a643a0b08c90f0dc35159d0c16942339
YukihiraDeve <82152262+YukihiraDeve@users.noreply.github.com>
2025-06-12 11:12:10 +0200

    Update README with instructions

---
+++ LiquidGlass/src/components/LiquidGlassEffect.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './LiquidGlassEffect.css'
+
+const LiquidGlassEffect = () => {
+  return (
+    <div className="liquid-container">
+      <div className="liquid" />
+      <div className="content">
+        <h1>Liquid Glass</h1>
+        <p>Apple inspired visual effect recreated in React</p>
+      </div>
+    </div>
+  )
+}
+
+export default LiquidGlassEffect
